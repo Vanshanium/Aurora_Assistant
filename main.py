a05e4da64ad82e4840fc72b9bf6dd72bf8dfba38
Venzene <vansh.singh8272@gmail.com>
2024-01-15 17:04:03 +0530

    The main function is done and I am pushing the code

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-import back_functions
+import functions
 
-import back_functions
-print("this")
+
+
+# This fine tunes the gpt and produce perfect Responces
+
+functions.response_tuner()
+
+
+"""
+
+This While loops goes on till the code is exited.
+It constantly takes the commands and produce responces.
+
+"""
+
+
+forever = True
+
+while(forever):
+
+    functions.voice_command(6)
+
+    command = functions.recognize_speech()
+
+    response = functions.get_responce(command)
+
+    functions.read_outload(response,engine="openai")
+
+
+
